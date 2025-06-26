3559fae84f852790af48aaed13b57b790d348f6e
linhengji1 <linhengji1@jd.com>
2025-06-26 21:05:58 +0800

    搭建4

--- client/src/pages/home/game.tsx
+++ client/src/pages/home/game.tsx
@@ -2,8 +2,21 @@
  * @Description: 首页-比赛
  * @Date: 2025-06-26 16:26:33
  */
-
+import { useEffect } from 'react';
+import Taro from '@tarojs/taro';
 const Game = () => {
+  useEffect(() => {
+    Taro.cloud.callFunction({
+      name: 'login',
+      data: {
+        userInfo: { /* 传递的用户信息 */ },
+      },
+    }).then(res => {
+      console.log('云函数调用成功:', res.result);
+    }).catch(err => {
+      console.error('云函数调用失败:', err);
+    });
+  }, []);
   return (
     <div>
       456
