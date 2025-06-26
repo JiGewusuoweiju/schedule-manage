5845e6d5768f189b1a544b01cfed86b0ebe32c1b
linhengji1 <linhengji1@jd.com>
2025-06-26 21:06:58 +0800

    搭建5

---
+++ cloudfunctions/login/index.js
@@ -0,0 +1,16 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  console.log("🐡🐟🐠✨️✨️✨️✨️✨️✨️✨️✨️✨️✨️111: ", 111);
+  return {
+    event,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
+  }
+}
