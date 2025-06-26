0a715b1eac87be2e758ac3ecb12e3bb028058ab4
linhengji1 <linhengji1@jd.com>
2025-06-26 18:05:39 +0800

    搭建3

---
+++ client/config/env.ts
@@ -0,0 +1,2 @@
+export const CLOUD_SERVICE = "cloud1-4ge0atth2e791111"; // 测试环境
+// export const CLOUD_SERVICE = "cloud1-4ge0atth2e791111"; // 生产环境
--- client/src/app.ts
+++ client/src/app.ts
@@ -1,11 +1,22 @@
 import { useEffect } from 'react'
 import { useDidShow, useDidHide } from '@tarojs/taro'
+import Taro from '@tarojs/taro'
+import { CLOUD_SERVICE } from "../config/env";
 // 全局样式
 import './app.less'
 
 function App(props) {
-  // 可以使用所有的 React Hooks
-  useEffect(() => {})
+  // 初始化云开发环境
+  useEffect(() => {
+    if (!Taro.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      Taro.cloud.init({
+        env: CLOUD_SERVICE, // 设置云函数调用环境
+        traceUser: true,    // 是否在将用户访问记录到用户管理中，在控制台中可见
+      })
+    }
+  }, [])
 
   // 对应 onShow
   useDidShow(() => {})
